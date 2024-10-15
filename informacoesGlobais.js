49339d0d8e05db6672a0c78b4a410c55a49d612c
maariaazinha <118922454+maariaazinha@users.noreply.github.com>
2024-10-15 09:35:47 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,7 +1,7 @@
 const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoesGlobais(){
-    const res =await feach(url)
+    const res =await fetch(url)
     const dados=await res.json()
     const pessoasConectadas=(dados.total_pessoas_conectadas/1e9)
     const pessoasNoMundo=(dados.total_pessoas_mundo/1e9)
